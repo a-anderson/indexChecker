73d435592b89bf13f4c395221fad78c7a6c168eb
Ashley <a.anderson16@student.unimelb.edu.au>
2020-04-20 21:27:09 +1000

    finished layout plan, index collision funcitons working

--- app.R
+++ app.R
@@ -1,56 +1,181 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(DT)
 library(rhandsontable)
+
+# Construct table for index selection
+tableConstructor <- function() {
+    rows = c("A", "B", "C", "D", "E", "F", "G", "H")
+    plate = data.frame(matrix(ncol=12, nrow=8), row.names=rows)
+    colnames(plate) <- seq(1, 12)
+    plate[is.na(plate)] = FALSE
+    return(plate)
+}
+
+hammingDistance <- function(string1, string2) {
+    # Calculate the hamming distance between 2 strings
+    
+    minLength = min(nchar(string1), nchar(string2))
+    
+    # split strings into parsable characters
+    split1 <- strsplit(string1, "")[[1]]
+    split2 <- strsplit(string2, "")[[1]]
+    
+    distance = sum(!split1[1:minLength] == split2[1:minLength])
+    
+    return(distance)
+}
+
+checkIndex <- function(indexTable) {
+    # Compare differences between all indices in a table
+    
+    outputDF = data.frame(index1=integer(), 
+                          index2=integer(), 
+                          sequence1=character(), 
+                          sequence2=character(),
+                          distance=integer(),
+                          stringsAsFactors=FALSE)
+    
+    numIndices = nrow(indexTable)
+    
+    # loop over all indices
+    for (i in 1:(numIndices-1)) {
+        index1 = trimws(indexTable[i,1])
+        
+        # ignore blank entry
+        if (nchar(index1) == 0 ) next
+        
+        for (j in (i+1):numIndices) {
+            
+            index2 = trimws(indexTable[j,1])
+            
+            # ignore blank entry
+            if (nchar(index2) == 0 ) next
+            
+            distance = hammingDistance(index1, index2)
+            
+            if (distance < 3) {
+                collision <- c(index1=i, 
+                               index2=j, 
+                               sequence1=index1, 
+                               sequence2=index2, 
+                               distance=distance)
+                
+                outputDF[nrow(outputDF) + 1, ] = collision
+            }
+        }
+    }
+    return(outputDF)
+}
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     
     tags$head(
-        tags$link( 
-            rel="stylesheet",
-            type="text/css",
-            href="style.css"
-        )
+        tags$link(rel = "stylesheet",
+                  type = "text/css",
+                  href = "style.css"),
+        tags$h1("Dawson Lab"),
     ),
-    tags$h1("Dawson Lab"),
-    tags$h4("Index Compatibility Checker"),
+    
+    tags$div(
+        id="row-groups",
+        tags$h3("Index Compatibility Checker")
+    ),
+    
     
     fluidRow(
         column(3, selectInput(inputId="kit", label="Kit", choices=c("TakaraBio", "Other"))),
         column(2, selectInput(inputId="set", "Set", choices=c("A", "B", "C", "D"))),
         column(3, selectInput(inputId="machine", label="Sequencer", choices=c("NextSeq", "MiSeq"))),
     ),
-   
-    tableOutput("kit_indices"),
     
-    sidebarPanel(
+    tags$div(
+        id="row-groups",
+        fluidRow(
+            column(9, rHandsontableOutput("kit_indices", width = 400)),
+            column(3, actionButton("add", "Add Indices to List"))
+        ),
+    ),
+    
+    tags$div(
+        id="row-groups",
+        tags$h3(" ")
+    ),
+    
+    tags$div(
+        id="row-groups",
         actionButton("check", "Check Index"),
-        hr(),
-        
-        conditionalPanel(condition = "true", rHandsontableOutput("hot", width = 400))
-    ),
+        tags$div(
+            tags$h3(" ")
+        ),
+        fluidRow(
+            column(4, conditionalPanel(condition = "true", rHandsontableOutput("index_in", width = 300))),
+            column(8, rHandsontableOutput("collisions"))
+        ),
+    )
     
-    mainPanel(
-        rHandsontableOutput("collisions", width = 500)
-    ),
     
-    downloadButton(outputId="download", label="Download")
+    # downloadButton(outputId="download", label="Download")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    
+    output$kit_indices <- renderRHandsontable(
+        rhandsontable(tableConstructor(), width = 800) %>%
+            hot_cols(colWidths = 40)
+    )
+    
+    indexIn = reactive({
+        if (!is.null(input$index_in)) {
+            indexDF = hot_to_r(input$index_in)
+        } else {
+            indexDF = data.frame(index = rep("",50), stringsAsFactors = FALSE)
+        }
+        return(indexDF)
+    })
+    
+    output$index_in <- renderRHandsontable(
+        rhandsontable(indexIn(), width = 400) %>%
+            hot_cols(colWidths = 200)
+    )
+    
+    # triggered by check button
+    checkResult = reactive({
+        input$check
+        isolate({
+            result=list()
+            result$collisionCount=0
+            result$collisionTable=data.frame()
+            
+            if (!is.null(input$collisions)) {
+                # get input data
+                inputTable=indexIn()
+                
+                # run collsion check
+                collisionTable = checkIndex(inputTable)
+                
+                result$collisionCount = nrow(collisionTable)
+                
+                if (result$collisionCount == 0) {
+                    collisionTable = data.frame(result=c("No collisions"))
+                }
+                result$collisionTable = collisionTable
+            }
+            return(result)
+        })
+    })
+    
+    output$collisions <- renderRHandsontable({
+        outputTable = checkResult()
+        rhandsontable(outputTable$collisionTable, readOnly=TRUE)
+    })
+    
     
 }
+    
+
+    
 
 # Run the application 
 shinyApp(ui = ui, server = server)
--- methods.R
+++ methods.R
@@ -22,10 +22,47 @@ hammingDistance <- function(string1, string2) {
   return(distance)
 }
 
+checkIndex <- function(indexTable) {
+  # Compare differences between all indices in a table
+  
+  outputDF = data.frame(index1=integer(), 
+                        index2=integer(), 
+                        sequence1=character(), 
+                        sequence2=character(),
+                        distance=integer(),
+                        stringsAsFactors=FALSE)
+  
+  numIndices = nrow(indexTable)
+  
+  # loop over all indices
+  for (i in 1:numIndices-1) {
+    index1 = trimws(indexTable[i,1])
+    
+    # ignore blank entry
+    if (nchar(index1) == 0 ) next
+    
+    for (j in i+1:numIndices) {
+      
+      index2 = trimws(indexTable[j,1])
+      
+      # ignore blank entry
+      if (nchar(index2) == 0 ) next
+      
+      distance = hammingDistance(index1, index2)
+      
+      if (distance < 3) {
+        collision <- c(index1=i, index2=j, sequence1=index1, sequence2=index2, distance=distance)
+        outputDF[nrow(outputDF) + 1, ] = collision
+      }
+    }
+  }
+  return(outputDF)
+}
+
 tableConstructor <- function() {
   rows = c("A", "B", "C", "D", "E", "F", "G", "H")
-  plate = data.frame(matrix(ncol=16, nrow=8), row.names=rows)
-  colnames(plate) <- seq(1, 16)
+  plate = data.frame(matrix(ncol=12, nrow=8), row.names=rows)
+  colnames(plate) <- seq(1, 12)
   plate[is.na(plate)] = FALSE
   return(plate)
 }
