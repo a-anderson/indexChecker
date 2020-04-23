dedad83ba4d6aab27e75e0554e8857ba2f843458
Ashley <a.anderson16@student.unimelb.edu.au>
2020-04-23 15:05:13 +1000

    updated readme file

--- install.R
+++ install.R
@@ -1,7 +1,7 @@
-# Run this file before running the app - it will install the packages you need.
-# This might take a while, the console will show a > symbol at the bottom when the installation is finished.
-# You will only need to run it once.
+# Run the following commands in R Studio before running the app - they will install the packages you need.
 
 install.packages("dplyr")
 install.packages("shiny")
 install.packages("rhandsontable")
+
+# This might take a while, but you will only need to do this once. When the installation is finished a > symbol and flashing cursor will show at the bottom of the console.
